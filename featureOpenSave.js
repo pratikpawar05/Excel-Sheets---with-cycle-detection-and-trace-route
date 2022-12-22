e82835d12b47fdf2e00cba50a0c2e54dc1dd73e3
Pratik Pawar <icanpratikpawar@gmail.com>
2022-12-22 23:33:13 +0530

    Added open and download sheet feature

---
+++ featureOpenSave.js
@@ -0,0 +1,44 @@
+let downloadBtn = document.querySelector(".download");
+let openBtn = document.querySelector(".open");
+
+// Download Task
+downloadBtn.addEventListener("click", (e) => {
+  let jsonData = JSON.stringify([sheetDB]);
+  let file = new Blob([jsonData], { type: "application/json" });
+
+  let a = document.createElement("a");
+  a.href = URL.createObjectURL(file);
+  a.download = "SheetData.json";
+  a.click();
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+});
+
+// Open task (upload)
+openBtn.addEventListener("click", (e) => {
+  // Opens file explorer
+  let input = document.createElement("input");
+  input.setAttribute("type", "file");
+  input.click();
+
+  input.addEventListener("change", (e) => {
+    let fr = new FileReader();
+    let files = input.files;
+    let fileObj = files[0];
+
+    fr.readAsText(fileObj);
+    fr.addEventListener("load", (e) => {
+      let readSheetData = JSON.parse(fr.result);
+
+      // Basic sheet with default data will be created
+      addSheetBtn.click();
+
+      // SheetDB
+      sheetDB = readSheetData[0];
+      collectedSheetDB[collectedSheetDB.length - 1] = sheetDB;
+      handleSheetProperties();
+    });
+  });
+});
