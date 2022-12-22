1f1f610f035ba8c3ee1bc175c42c668fd949333f
Pratik Pawar <icanpratikpawar@gmail.com>
2022-12-22 17:10:13 +0530

    Copy Feature Added

--- featureCutCopyPaste.js
+++ featureCutCopyPaste.js
@@ -15,3 +15,54 @@ for (let i = 0; i < rows; i++) {
 let copyBtn = document.querySelector(".copy");
 let cutBtn = document.querySelector(".cut");
 let pasteBtn = document.querySelector(".paste");
+
+let rangeStorage = [];
+function handleSelectedCells(cell) {
+  cell.addEventListener("click", (e) => {
+    // Select cells range work
+    if (!ctrlKey) return;
+    if (rangeStorage.length >= 2) {
+      defaultSelectedCellsUI();
+      rangeStorage = [];
+    }
+
+    // UI
+    cell.style.border = "3px solid #218c74";
+
+    let rid = Number(cell.getAttribute("rid"));
+    let cid = Number(cell.getAttribute("cid"));
+    rangeStorage.push([rid, cid]);
+    console.log(rangeStorage);
+  });
+}
+function defaultSelectedCellsUI() {
+  for (let i = 0; i < rangeStorage.length; i++) {
+    let cell = document.querySelector(
+      `.cell[rid="${rangeStorage[i][0]}"][cid="${rangeStorage[i][1]}"]`
+    );
+    cell.style.border = "1px solid lightgrey";
+  }
+}
+
+let copyData = [];
+
+copyBtn.addEventListener("click", (e) => {
+  if (rangeStorage.length < 2) return;
+  copyData = [];
+  let [strow, stcol, endrow, endcol] = [
+    rangeStorage[0][0],
+    rangeStorage[0][1],
+    rangeStorage[1][0],
+    rangeStorage[1][1],
+  ];
+
+  for (let i = strow; i <= endrow; i++) {
+    let copyRow = [];
+    for (let j = stcol; j <= endcol; j++) {
+      let cellProp = sheetDB[i][j];
+      copyRow.push(cellProp);
+    }
+    copyData.push(copyRow);
+  }
+  defaultSelectedCellsUI();
+});
